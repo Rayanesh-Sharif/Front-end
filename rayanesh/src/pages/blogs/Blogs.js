bca58f8ec7719ff924759af7eb2249e1b49be35d
Arash <arashyadegari0402@gmail.com>
2022-08-16 16:09:51 +0430

    add animation for blog

--- rayanesh/src/pages/blogs/Blogs.js
+++ rayanesh/src/pages/blogs/Blogs.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PopularBlogsSection from '../../components/PopularBlogsSection';
-import { Parallax, ParallaxLayer } from '@react-spring/web/parallax'
+import { Parallax, ParallaxLayer } from '@react-spring/parallax'
 
 function Blogs(props) {
     return (
@@ -8,30 +8,24 @@ function Blogs(props) {
             <Parallax pages={2} style={{ top: '0', left: '0' }}>
                 <ParallaxLayer
                     offset={0}
-                    speed={2.5}
-                    style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                    <p>Scroll down</p>
+                    speed={2.5}>
+
+                    <PopularBlogsSection blogs={[
+                        { id: 1, title: "Blog 1", image: "https://picsum.photos/seed/picsum/1000/1000?random=1" },
+                        { id: 2, title: "Blog 2", image: "https://picsum.photos/seed/picsum/1000/1000?random=2" },
+                        { id: 3, title: "Blog 3", image: "https://picsum.photos/seed/picsum/1000/1000?random=3" }
+                    ]} />
                 </ParallaxLayer>
 
-                <ParallaxLayer offset={1} speed={2} style={{ backgroundColor: '#ff6d6d' }} />
+                <ParallaxLayer offset={1} speed={2} style={{ backgroundColor: '#21A362' }} />
 
                 <ParallaxLayer
                     offset={1}
-                    speed={0.5}
-                    style={{
-                        display: 'flex',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        color: 'white',
-                    }}>
-                    <p>Scroll up</p>
+                    speed={0.5}>
+                    
                 </ParallaxLayer>
             </Parallax>
-            <PopularBlogsSection blogs={[
-                { id: 1, title: "Blog 1", image: "https://picsum.photos/seed/picsum/1000/1000?random=1" },
-                { id: 2, title: "Blog 2", image: "https://picsum.photos/seed/picsum/1000/1000?random=2" },
-                { id: 3, title: "Blog 3", image: "https://picsum.photos/seed/picsum/1000/1000?random=3" }
-            ]} />
+
         </div>
     );
 }

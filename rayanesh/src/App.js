25b78f636d0a93460b79ffb127d4d35a8a4d0e57
Arash <arashyadegari0402@gmail.com>
2022-09-01 23:32:40 +0430

    add rich text editor

--- rayanesh/src/App.js
+++ rayanesh/src/App.js
@@ -4,18 +4,20 @@ import NotFound from "./pages/notFound/NotFound";
 import Blogs from "./pages/blogs/Blogs";
 import NavbarSection from "./components/NavbarSection/NavbarSection";
 import 'mdb-react-ui-kit/dist/css/mdb.min.css'
+import RichTextEditor from "./components/RichTextEditor/RichTextEditor";
 
 function App() {
     return (
         <Router>
             <NavbarSection loggedIn={false}  />
-            <div className="App">
+            <RichTextEditor />
+{/*            <div className="App">
                 <Routes>
                     <Route path="/" element={<LandingPage />} />
                     <Route path="/blogs" element={<Blogs />} />
                     <Route path="*" element={<NotFound />} />
                 </Routes>
-            </div>
+            </div>*/}
         </Router>
     )
 }
---
+++ rayanesh/src/components/RichTextEditor/RichTextEditor.js
@@ -0,0 +1,66 @@
+import React from "react";
+import { Editor, EditorState, RichUtils } from "draft-js";
+
+export default class RichTextEditor extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            editorState: EditorState.createEmpty()
+        };
+    }
+
+    onChange = editorState => {
+        this.setState({
+            editorState
+        });
+    };
+
+    handleKeyCommand = command => {
+        const newState = RichUtils.handleKeyCommand(
+            this.state.editorState,
+            command
+        );
+        if (newState) {
+            this.onChange(newState);
+            return "handled";
+        }
+        return "not-handled";
+    };
+
+    onUnderlineClick = () => {
+        this.onChange(
+            RichUtils.toggleInlineStyle(this.state.editorState, "UNDERLINE")
+        );
+    };
+
+    onBoldClick = () => {
+        this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, "BOLD"));
+    };
+
+    onItalicClick = () => {
+        this.onChange(
+            RichUtils.toggleInlineStyle(this.state.editorState, "ITALIC")
+        );
+    };
+
+    render() {
+        return (
+            <div className= 'align-center'>
+                <button onClick={this.onUnderlineClick}>U</button>
+                <button onClick={this.onBoldClick}>
+                    <b>B</b>
+                </button>
+                <button onClick={this.onItalicClick}>
+                    <em>I</em>
+                </button>
+                <div className="editors">
+                    <Editor
+                        editorState={this.state.editorState}
+                        handleKeyCommand={this.handleKeyCommand}
+                        onChange={this.onChange}
+                    />
+                </div>
+            </div>
+        );
+    }
+}

201da2ce5a7fcc9bf02c04223db0f14f67299d6a
Muhammad Khosravi <mohamad138121@gmail.com>
2022-09-13 21:04:00 +0430

    fix style of the alert

--- rayanesh/src/components/BlogInfoAlert.jsx
+++ rayanesh/src/components/BlogInfoAlert.jsx
@@ -1,39 +1,39 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Alert from '@mui/material/Alert';
-import IconButton from '@mui/material/IconButton';
-import Collapse from '@mui/material/Collapse';
-import Button from '@mui/material/Button';
-import CloseIcon from '@mui/icons-material/Close';
-import {Typography} from "@mui/material";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Alert from "@mui/material/Alert";
+import IconButton from "@mui/material/IconButton";
+import Collapse from "@mui/material/Collapse";
+import Button from "@mui/material/Button";
+import CloseIcon from "@mui/icons-material/Close";
+import { Typography } from "@mui/material";
 
 export default function BlogInfoAlert() {
-    const [open, setOpen] = React.useState(true);
+  const [open, setOpen] = React.useState(true);
 
-    return (
-        <Box sx={{ width: '100%', marginTop: 2}}>
-            <Collapse in={open}>
-                <Alert
-                    severity={'info'}
-                    action={
-                        <IconButton
-                            aria-label="close"
-                            color="inherit"
-                            size="small"
-                            onClick={() => {
-                                setOpen(false);
-                            }}
-                        >
-                            <CloseIcon fontSize="inherit" />
-                        </IconButton>
-                    }
-                    sx={{ mb: 2 }}
-                >
-                    <Typography >
-                        اگه برای نوشتن بلاگ کمک میخوای یه سر به اینجا بزن.
-                    </Typography>
-                </Alert>
-            </Collapse>
-        </Box>
-    );
+  return (
+    <Box sx={{ width: "100%", marginTop: 2 }}>
+      <Collapse in={open}>
+        <Alert
+          severity={"info"}
+          action={
+              <IconButton
+                aria-label="close"
+                color="inherit"
+                size="small"
+                onClick={() => {
+                  setOpen(false);
+                }}
+              >
+                <CloseIcon fontSize="inherit" />
+              </IconButton>
+          }
+          sx={{ mb: 2, flexDirection: "row-reverse", "& .MuiAlert-action": {marginRight: 'auto', marginLeft: 0} }}
+        >
+          <Typography sx={{direction: 'rtl', marginRight: 2}}>
+            اگه برای نوشتن بلاگ کمک میخوای یه سر به اینجا بزن.
+          </Typography>
+        </Alert>
+      </Collapse>
+    </Box>
+  );
 }

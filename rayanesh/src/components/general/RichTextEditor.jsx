8489138e884fb743044153c605dbe223502852d4
Arash <arashyadegari0402@gmail.com>
2022-09-19 10:02:14 +0430

    debug duplicate editor component | save editor object

--- rayanesh/src/components/general/RichTextEditor.jsx
+++ rayanesh/src/components/general/RichTextEditor.jsx
@@ -1,6 +1,5 @@
 import EditorJS from "@editorjs/editorjs";
 import Header from "@editorjs/header";
-import { useEffect, useState } from "react";
 import AlignmentTuneTool from "editorjs-text-alignment-blocktune";
 import ImageTool from "@editorjs/image";
 import Delimiter from "@editorjs/delimiter";
@@ -11,159 +10,168 @@ import CodeTool from "@editorjs/code";
 import ToggleBlock from "editorjs-toggle-block";
 
 let isCreated = false;
-const initEditor = ({ id, data, isReadOnly }) => {
-  if (isCreated) return null;
-  isCreated = true;
-  return new EditorJS({
-    holder: id,
-    placeholder: "هر آنچه دل تنگت خواست",
-    readOnly: isReadOnly,
-    tools: {
-      header: {
-        class: Header,
-        inlineToolbar: true,
-        title: "عنوان",
-        tunes: ["aligner"],
-        config: {
-          placeholder: "عنوان",
-          levels: [1, 2, 3],
-          defaultLevel: 2,
-        },
-      },
-      paragraph: {
-        class: Paragraph,
-        inlineToolbar: true,
-        tunes: ["aligner"],
-      },
-      list: {
-        class: List,
-        inlineToolbar: true,
-      },
-      code: {
-        class: CodeTool,
-      },
-      alert: {
-        class: Alert,
-        inlineToolbar: true,
-        config: {
-          messagePlaceholder: "متن پیام",
-          defaultType: "success",
-        },
-      },
-      image: {
-        class: ImageTool,
-        inlineToolbar: true,
-        config: {
-          endpoints: {},
-        },
-      },
-      toggle: {
-        class: ToggleBlock,
-      },
-      delimiter: {
-        class: Delimiter,
-        inlineToolbar: true,
-      },
-      aligner: {
-        class: AlignmentTuneTool,
-        config: {
-          default: "right",
-          blocks: {
-            header: "right",
-            list: "right",
-            code: "left",
-          },
-        },
-      },
-    },
-
-    i18n: {
-      direction: "rtl",
-      messages: {
-        ui: {
-          toolbar: {
-            toolbox: {
-              Add: "افزودن",
-            },
-          },
-          inlineToolbar: {
-            converter: {
-              "Convert to": "تبدیل به",
-            },
-          },
-          blockTunes: {
-            toggler: {
-              "Click to tune": "برای تنظیم کلیک کنید",
-            },
-          },
-        },
-        toolNames: {
-          Heading: "عنوان",
-          List: "لیست",
-          Text: "متن",
-          Image: "تصویر",
-          Delimiter: "خط تقسیم",
-          Bold: "درشت",
-          Italic: "خمیده",
-          Link: "لینک",
-          Filter: "فیلتر",
-          Alert: "هشدار",
-          Code: "کد",
-          Toggle: "تغییر",
-        },
+let editor = null;
+const initEditor = ({id, data, isReadOnly}) => {
+    if (isCreated) return null;
+    isCreated = true;
+    editor = new EditorJS({
+        holder: id,
+        placeholder: "هر آنچه دل تنگت خواست",
+        readOnly: isReadOnly,
         tools: {
-          heading: {
-            title: "عنوان",
-          },
-          text: {
-            title: "متن",
-          },
-          image: {
-            title: "تصویر",
-          },
-          delimiter: {
-            title: "خط تقسیم",
-          },
-          link: {
-            title: "لینک",
-          },
-          filter: {
-            title: "فیلتر",
-          },
-          list: {
-            title: "لیست",
-          },
-          alert: {
-            title: "هشدار",
-          },
-          code: {
-            title: "کد",
-          },
-          toggle: {
-            Toggle: "تغییر بلوک",
-            "Empty toggle. Click or drop blocks inside.":
-              "تغییر خالی. بلوک ها را درونش بکشید یا کلیک کنید",
-          },
+            header: {
+                class: Header,
+                inlineToolbar: true,
+                title: "عنوان",
+                tunes: ["aligner"],
+                config: {
+                    placeholder: "عنوان",
+                    levels: [1, 2, 3],
+                    defaultLevel: 2,
+                },
+            },
+            paragraph: {
+                class: Paragraph,
+                inlineToolbar: true,
+                tunes: ["aligner"],
+            },
+            list: {
+                class: List,
+                inlineToolbar: true,
+            },
+            code: {
+                class: CodeTool,
+            },
+            alert: {
+                class: Alert,
+                inlineToolbar: true,
+                config: {
+                    messagePlaceholder: "متن پیام",
+                    defaultType: "success",
+                },
+            },
+            image: {
+                class: ImageTool,
+                inlineToolbar: true,
+                config: {
+                    endpoints: {},
+                },
+            },
+            toggle: {
+                class: ToggleBlock,
+                config: {
+                    placeholder: "تاگل",
+                    defaultContent: "تاگل خالی. بلوک ها را درونش بکشید یا کلیک کنید"
+                }
+            },
+            delimiter: {
+                class: Delimiter,
+                inlineToolbar: true,
+            },
+            aligner: {
+                class: AlignmentTuneTool,
+                config: {
+                    default: "right",
+                    blocks: {
+                        header: "right",
+                        list: "right",
+                        code: "left",
+                    },
+                },
+            },
         },
-        blockTunes: {
-          delete: {
-            Delete: "حذف کن",
-          },
-          moveUp: {
-            "Move up": "بالا ببر",
-          },
-          moveDown: {
-            "Move down": "پایین ببر",
-          },
+
+        i18n: {
+            direction: "rtl",
+            messages: {
+                ui: {
+                    toolbar: {
+                        toolbox: {
+                            Add: "افزودن",
+                        },
+                    },
+                    inlineToolbar: {
+                        converter: {
+                            "Convert to": "تبدیل به",
+                        },
+                    },
+                    blockTunes: {
+                        toggler: {
+                            "Click to tune": "برای تنظیم کلیک کنید",
+                        },
+                    },
+                },
+                toolNames: {
+                    Heading: "عنوان",
+                    List: "لیست",
+                    Text: "متن",
+                    Image: "تصویر",
+                    Delimiter: "خط تقسیم",
+                    Bold: "درشت",
+                    Italic: "خمیده",
+                    Link: "لینک",
+                    Filter: "فیلتر",
+                    Alert: "هشدار",
+                    Code: "کد",
+                    Toggle: "تغییر",
+                },
+                tools: {
+                    heading: {
+                        title: "عنوان",
+                    },
+                    text: {
+                        title: "متن",
+                    },
+                    image: {
+                        title: "تصویر",
+                    },
+                    delimiter: {
+                        title: "خط تقسیم",
+                    },
+                    link: {
+                        title: "لینک",
+                    },
+                    filter: {
+                        title: "فیلتر",
+                    },
+                    list: {
+                        title: "لیست",
+                    },
+                    alert: {
+                        title: "هشدار",
+                    },
+                    code: {
+                        title: "کد",
+                    }
+                },
+                blockTunes: {
+                    delete: {
+                        Delete: "حذف کن",
+                    },
+                    moveUp: {
+                        "Move up": "بالا ببر",
+                    },
+                    moveDown: {
+                        "Move down": "پایین ببر",
+                    },
+                },
+            },
+            data: data,
         },
-      },
-      data: data,
-    },
-  });
+    });
+    return editor;
 };
 
-const GetEditor = ({ data, isReadOnly, id }) => {
-  initEditor({ data, isReadOnly, id });
-  return <div style={{ marginTop: "2%" }} id={id} />;
+export const saveEditor = () => {
+
+    return editor.save();
+}
+
+
+const RichTextEditor = ({data, isReadOnly, id}) => {
+    initEditor({data, isReadOnly, id});
+    return <div style={{marginTop: "2%"}} id={id}/>;
 };
-export default GetEditor;
+
+
+export default RichTextEditor;

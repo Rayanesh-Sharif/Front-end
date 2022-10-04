7f2f2c136d308573217f9f48b8d868d640538d0a
Arash <arashyadegari0402@gmail.com>
2022-10-04 22:04:32 +0330

    add singIn/logIn button

--- rayanesh/src/components/NavbarSection.jsx
+++ rayanesh/src/components/NavbarSection.jsx
@@ -9,7 +9,6 @@ import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import {images} from "../constants";
 import {styled} from "@mui/material/styles";
@@ -33,30 +32,37 @@ const pages = [
         path: '/blogs',
     }
 ];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 const ColorAppBar = styled(AppBar)(({theme}) => ({
     backgroundColor: '#14937e'
 }));
-const ResponsiveAppBar = () => {
+
+
+const SignUpLoginButton = styled(Button)(
+    {
+        color: '#14937e',
+        backgroundColor: 'white',
+        borderColor: 'white',
+        '&:hover': {
+            backgroundColor: 'white',
+            borderColor: 'white'
+        }
+    }
+)
+
+const NavbarSection = ({loggedIn = 'false'}) => {
+    console.log('is log in : ', loggedIn)
     const [anchorElNav, setAnchorElNav] = React.useState(null);
-    const [anchorElUser, setAnchorElUser] = React.useState(null);
     const navigate = useNavigate();
 
     const handleOpenNavMenu = (event) => {
         setAnchorElNav(event.currentTarget);
-    };
-    const handleOpenUserMenu = (event) => {
-        setAnchorElUser(event.currentTarget);
     };
 
     const handleCloseNavMenu = () => {
         setAnchorElNav(null);
     };
 
-    const handleCloseUserMenu = () => {
-        setAnchorElUser(null);
-    };
 
     return (
         <ColorAppBar dir={'rtl'} position="static">
@@ -67,9 +73,10 @@ const ResponsiveAppBar = () => {
                         height='30'
                         src={images.Logo}
                         alt={'رایانش'}
+                        style={{marginLeft: '1rem'}}
                     />
 
-
+                    {/* for extra-small size screens*/}
                     <Box sx={{marginRight: 1, flexGrow: 1, display: {xs: 'flex', md: 'none'}}}>
                         <IconButton
                             size="large"
@@ -111,6 +118,9 @@ const ResponsiveAppBar = () => {
                                 ))}
                         </Menu>
                     </Box>
+
+
+                    {/* for md and bigger size screens*/}
                     <Box sx={{flexGrow: 1, display: {xs: 'none', md: 'flex'}}}>
                         {pages?.map((item) => (
                             <Button
@@ -127,38 +137,28 @@ const ResponsiveAppBar = () => {
                         ))}
                     </Box>
 
-                    <Box sx={{flexGrow: 0}}>
-                        <Tooltip title="Open settings">
-                            <IconButton onClick={handleOpenUserMenu} sx={{p: 0}}>
-                                <Avatar alt="Bomb" src="/static/images/avatar/2.jpg"/>
-                            </IconButton>
-                        </Tooltip>
-                        <Menu
-                            sx={{mt: '45px'}}
-                            id="menu-appbar"
-                            anchorEl={anchorElUser}
-                            anchorOrigin={{
-                                vertical: 'top',
-                                horizontal: 'right',
-                            }}
-                            keepMounted
-                            transformOrigin={{
-                                vertical: 'top',
-                                horizontal: 'right',
-                            }}
-                            open={Boolean(anchorElUser)}
-                            onClose={handleCloseUserMenu}
-                        >
-                            {settings?.map((setting) => (
-                                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                                    <Typography textAlign="center">{setting}</Typography>
-                                </MenuItem>
-                            ))}
-                        </Menu>
+                    <Box>
+                        <SignUpLoginButton color={'success'} variant='contained' onClick = {
+                            () => {
+                                navigate('/signup');
+                            }
+                        }>
+                            {loggedIn &&
+                                <Avatar
+                                    alt="something shit"
+                                    src="/static/images/avatar/2.jpg"
+                                />
+                            }
+                            {!loggedIn &&
+                                <Typography variant={'body1'}>
+                                    ثبت نام/ورود
+                                </Typography>
+                            }
+                        </SignUpLoginButton>
                     </Box>
                 </Toolbar>
             </Container>
         </ColorAppBar>
     );
 };
-export default ResponsiveAppBar;
+export default NavbarSection;

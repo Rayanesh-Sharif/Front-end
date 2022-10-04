3a072c15cffac462ad73cc4a3c2bebe1174742c4
Arash <arashyadegari0402@gmail.com>
2022-10-04 22:00:24 +0330

    add background for signup / login page

--- rayanesh/src/constants/images.js
+++ rayanesh/src/constants/images.js
@@ -1,7 +1,9 @@
 import Logo from '../assets/pics/logo.png'
 import View from '../assets/pics/view.jpg'
-
+import Curve from '../assets/pics/signup-background.png'
 export default {
     Logo,
     View,
+    Curve
+
 } 

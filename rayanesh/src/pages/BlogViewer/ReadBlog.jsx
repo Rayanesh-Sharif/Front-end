f4d274deff0f312ad3ee835f7f0dc71d566deab2
Muhammad Khosravi <mohamad138121@gmail.com>
2022-10-05 23:05:23 +0330

    fix ui bug in signup

--- rayanesh/src/pages/BlogViewer/ReadBlog.jsx
+++ rayanesh/src/pages/BlogViewer/ReadBlog.jsx
@@ -1,34 +1,35 @@
-import {Container} from "@mui/material";
+import { Container } from "@mui/material";
 import RichTextEditor from "../../components/general/RichTextEditor";
-import {useParams} from "react-router-dom"
+import { useParams } from "react-router-dom";
 import useGetSinglePost from "../../hooks/useGetSinglePost";
 import useGetSingleUser from "../../hooks/useGetSingleUser";
-import {useState} from "react";
-
-
+import { useEffect, useState } from "react";
 
 function ReadBlog() {
-    let params = useParams();
-    const id = params.id;
-    const {post, postStatus} = useGetSinglePost({postId: id});
-    const isEnable = postStatus === "success";
-    const {user, userStatus} = useGetSingleUser({userId: post?.user_id, isEnable: isEnable});
-    const isDataFetched = postStatus === "success" && userStatus === "success";
-    if (isDataFetched) {
-        return (
-            <Container sx={{fontFamily: 'Vazir'}}>
-                <RichTextEditor data={post.data} isReadOnly={true} id={id}/>
-            </Container>
-
-        )
-    }else {
-        return (
-            <Container>
-                <h1>loading...</h1>
-            </Container>
-
-        )
-    }
+  let params = useParams();
+  const id = params.id;
+  const { post, postStatus } = useGetSinglePost({ postId: id });
+  let isEnable = null;
+  isEnable = postStatus === "success";
+  const { user, userStatus } = useGetSingleUser({
+    userId: post?.user_id,
+    isEnable: isEnable,
+  });
+  const isDataFetched =
+    postStatus === "success" && userStatus === "success" && isEnable;
+  if (isDataFetched) {
+    return (
+      <Container sx={{ fontFamily: "Vazir" }}>
+        <RichTextEditor data={post?.data} isReadOnly={true} id={id} />
+      </Container>
+    );
+  } else {
+    return (
+      <Container>
+        <h1>loading...</h1>
+      </Container>
+    );
+  }
 }
 
 export default ReadBlog;

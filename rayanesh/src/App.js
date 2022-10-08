d40644e48f2a1e2b03973d8c88df770652dd9179
Arash <arashyadegari0402@gmail.com>
2022-10-08 10:14:31 +0330

    add login context provider

--- rayanesh/src/App.js
+++ rayanesh/src/App.js
@@ -1,17 +1,14 @@
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import LandingPage from "./pages/landingPage/LandingPage";
-import NavbarSection from "./components/NavbarSection";
-import SignUp from "./pages/signUp/SignUp";
 import React from "react";
 import {createTheme, ThemeProvider} from "@mui/material";
 import rtlPlugin from "stylis-plugin-rtl";
 import {CacheProvider} from "@emotion/react";
 import createCache from "@emotion/cache";
 import {prefixer} from "stylis";
-import Blogs from "./pages/blogs/Blogs";
-import ReadBlog from "./pages/BlogViewer/ReadBlog";
-import WriteBlog from "./pages/BlogViewer/WriteBlog";
-import NotFound from "./pages/notFound/NotFound";
+import AllRoutes from "./routes/AllRoutes";
+import {ToastContainer} from "react-toastify";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import LoginContextProvider from "./context/LoginContextProvider";
+import 'react-toastify/dist/ReactToastify.css';
 
 const cacheRtl = createCache({
     key: "muirtl",
@@ -25,8 +22,7 @@ const theme = createTheme({
     direction: 'rtl',
     typography: {
         fontFamily: [
-            'IRANSans',
-            'Vaazir'
+            'Vazir'
         ].join(','),
     },
 });
@@ -35,23 +31,21 @@ function App() {
     React.useLayoutEffect(() => {
         document.body.setAttribute("dir", "rtl");
     }, []);
+
+    const queryClient = new QueryClient();
     return (
-        <CacheProvider value={cacheRtl}>
-            <ThemeProvider theme={theme}>
-                <Router>
-                    <div className="App">
-                        <Routes>
-                            <Route path="/" element={<LandingPage/>}/>
-                            <Route path="/blogs" element={<Blogs/>}/>
-                            <Route path="/blogs/:id" element={<ReadBlog/>}/>
-                            <Route path="/blogs/write" element={<WriteBlog/>}/>
-                            <Route path="*" element={<NotFound/>}/>
-                            <Route path="/signup" element={<SignUp/>}/>
-                        </Routes>
-                    </div>
-                </Router>
-            </ThemeProvider>
-        </CacheProvider>
+        <div className="App">
+            <QueryClientProvider client={queryClient}>
+                <CacheProvider value={cacheRtl}>
+                    <ThemeProvider theme={theme}>
+                        <LoginContextProvider>
+                            <AllRoutes/>
+                        </LoginContextProvider>
+                        <ToastContainer/>
+                    </ThemeProvider>
+                </CacheProvider>
+            </QueryClientProvider>
+        </div>
     )
 }
 

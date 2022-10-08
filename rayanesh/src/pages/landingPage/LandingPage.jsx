408c764a5b4a093f2b168b1c30c79379707dced3
Arash <arashyadegari0402@gmail.com>
2022-10-08 13:45:59 +0330

    UNDER CONSTRUCTION

--- rayanesh/src/pages/landingPage/LandingPage.jsx
+++ rayanesh/src/pages/landingPage/LandingPage.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Page from "../../components/general/Page";
+import {Container} from "@mui/material";
+import ConstructionIcon from '@mui/icons-material/Construction';
 
 function LandingPage(props) {
 
     return (
         <Page>
-            <h1>مهسا رفتی و دنیا کمی زشت تر شد</h1>
+            <Container>
+                <h1>Under Construction <ConstructionIcon/></h1>
+            </Container>
         </Page>
     );
 }

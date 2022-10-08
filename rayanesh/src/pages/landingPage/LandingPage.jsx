4d13785e3119294383592630685e293376838a6f
Arash <arashyadegari0402@gmail.com>
2022-10-08 10:22:34 +0330

    wrap landing page

---
+++ rayanesh/src/pages/landingPage/LandingPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Page from "../../components/general/Page";
+
+function LandingPage(props) {
+
+    return (
+        <Page>
+            <h1>مهسا رفتی و دنیا کمی زشت تر شد</h1>
+        </Page>
+    );
+}
+
+export default LandingPage;
+

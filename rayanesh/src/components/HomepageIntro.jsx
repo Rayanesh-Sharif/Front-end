9812db423ac0027949e54f3075c9dfb0dda236b4
Arash <arashyadegari0402@gmail.com>
2022-10-12 11:59:19 +0330

    separate reusable elements

--- rayanesh/src/components/HomepageIntro.jsx
+++ rayanesh/src/components/HomepageIntro.jsx
@@ -1,72 +1,23 @@
 import React from 'react';
-import {Button, Container, Typography} from "@mui/material";
+import {
+    NoPaddingContainer,
+    ResponsiveBody,
+    ResponsiveHeader,
+    TextContainer
+} from "./general/IntroComponent";
 import {images} from "../constants";
-import {styled} from "@mui/material/styles";
-
-
-const mainContainerStyle = {
-    backgroundImage: `url(${images.GreenWave})`,
-    height: '80vh',
-    backgroundSize: 'cover',
-    display: 'flex',
-    padding: '0',
-    margin: '0',
-}
-
-const NoPaddingContainer = styled(Container)({
-    padding: '0',
-    margin: '0',
-    height: '80vh',
-    backgroundSize: 'cover',
-    display: 'flex',
-    backgroundImage: `url(${images.GreenWave})`,
-    minWidth: '100%',
-})
-
-const TextContainer = styled(Container)({
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: '100%',
-    width: '60%',
-    textAlign: 'center',
-})
-
-const HomepageButton = styled(Button)({
-    backgroundColor: '#ffffff',
-    color: '#038a79',
-    margin: '10px',
-    '&:hover': {
-        backgroundColor: '#038a79',
-        color: '#ffffff',
-    }
-})
-
 
 function HomepageIntro() {
     return (
-        <NoPaddingContainer>
+        <NoPaddingContainer sx={{backgroundImage: `url(${images.GreenWave})`,}}>
             <TextContainer>
-                <Typography fontWeight={'bold'} variant={'h2'} color={'#ffffff'} gutterBottom>
+                <ResponsiveHeader fontWeight={'bold'} variant={'h2'} color={'#ffffff'} gutterBottom>
                     محل قرارگیری یک جمله الهام بخش
-                </Typography>
-                <Typography variant={'h4'} color={'#ffffff'} gutterBottom>
+                </ResponsiveHeader>
+                <ResponsiveBody variant={'h4'} color={'#ffffff'} gutterBottom>
                     محل قرارگیری جمله اول که پرسشی است? و سپس محل قرار گیری جمله دوم که پاسخ به پرسش است. و در انتها یک
                     جمله با علامت تعجب برای نشان دادن اوج هیجان!!!
-                </Typography>
-
-                <Container>
-                    <HomepageButton size={'large'}>
-                        این دکمه را فشار ندهید
-                    </HomepageButton>
-
-                    <HomepageButton size={'large'}>
-                        این دکمه را هم فشار ندهید
-                    </HomepageButton>
-
-                </Container>
-
+                </ResponsiveBody>
             </TextContainer>
         </NoPaddingContainer>
     );
---
+++ rayanesh/src/components/general/IntroComponent.jsx
@@ -0,0 +1,68 @@
+import {styled} from "@mui/material/styles";
+import {Button, Container, Typography} from "@mui/material";
+
+
+export const NoPaddingContainer = styled(Container)({
+    padding: '0',
+    margin: '0',
+    height: '60vh',
+    backgroundSize: 'cover',
+    display: 'flex',
+    flexDirection: 'column',
+    minWidth: '100%',
+})
+
+export const TextContainer = styled(Container)({
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100%',
+    width: '60%',
+    padding: '0',
+    textAlign: 'center',
+})
+
+export const HomepageButton = styled(Button)({
+    backgroundColor: '#ffffff',
+    color: '#038a79',
+    margin: '10px',
+    '&:hover': {
+        backgroundColor: '#038a79',
+        color: '#ffffff',
+    }
+})
+
+
+// responsive typography for h1 variant
+export const ResponsiveHeader = styled(Typography)(({theme}) => ({
+    fontFamily: 'B Arshia',
+    [theme.breakpoints.down('sm')]: {
+        fontSize: '2.3rem',
+    },
+    [theme.breakpoints.up('sm')]: {
+        fontSize: '2.5rem',
+    },
+    [theme.breakpoints.up('md')]: {
+        fontSize: '3.5rem',
+    },
+    [theme.breakpoints.up('lg')]: {
+        fontSize: '4.5rem',
+    }
+}));
+
+export const ResponsiveBody = styled(Typography)(({theme}) => ({
+    fontFamily: 'B Arshia',
+    [theme.breakpoints.down('sm')]: {
+        fontSize: '1.3rem',
+    },
+    [theme.breakpoints.up('sm')]: {
+        fontSize: '1.6rem',
+    },
+    [theme.breakpoints.up('md')]: {
+        fontSize: '1.9rem',
+    },
+    [theme.breakpoints.up('md')]: {
+        fontSize: '2.2rem',
+    },
+}));

453d3518f4101a667d1cfa7f1b11d978a462e21d
Arash <arashyadegari0402@gmail.com>
2022-10-13 22:25:51 +0330

    refactor design name

--- rayanesh/src/constants/images.js
+++ rayanesh/src/constants/images.js
@@ -1,16 +1,16 @@
 import Logo from '../assets/pics/logo.png'
 import View from '../assets/pics/view.jpg'
 import Curve from '../assets/pics/signup-background.png'
-import Typewriter from '../assets/pics/typewriter.jpg'
-import GreenWave from '../assets/pics/homepage-background.png'
-import Rayanname from '../assets/pics/rayanname.jpg'
-import AboutUs from '../assets/pics/aboutus.webp'
+import Blogs from '../assets/pics/blogs.svg'
+import GreenWave from '../assets/pics/homepage-background.svg'
+import Rayanname from '../assets/pics/rayanname.svg'
+import AboutUs from '../assets/pics/aboutus.svg'
 import Magazine from '../assets/pics/magazine.png'
 export default {
     Logo,
     View,
     Curve,
-    Typewriter,
+    Blogs,
     GreenWave,
     Rayanname,
     AboutUs,
